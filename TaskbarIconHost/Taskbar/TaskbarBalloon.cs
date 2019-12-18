f287f443ed9d105894c61eb487222ab58abd46ef
dlebansais <dlebansais@gmail.com>
2019-12-18 12:23:26 +0100

    Fixed typos.

--- TaskbarIconHost/Taskbar/TaskbarBalloon.cs
+++ TaskbarIconHost/Taskbar/TaskbarBalloon.cs
@@ -4,14 +4,17 @@
     using System.Threading;
     using System.Windows.Forms;
 
+    /// <summary>
+    /// This class provides an API to display notifications to the user.
+    /// </summary>
     public static class TaskbarBalloon
     {
         #region Client Interface
         /// <summary>
         /// Display a notification in a taskbar balloon.
         /// </summary>
-        /// <param name="text">The text to show</param>
-        /// <param name="delay">The delay, in milliseconds</param>
+        /// <param name="text">The text to show.</param>
+        /// <param name="delay">The delay, in milliseconds.</param>
         public static void Show(string text, short delay = 5000)
         {
             try
