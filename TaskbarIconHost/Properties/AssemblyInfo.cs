abe4f6e4945f8e047c23e8d67c24a03cca36791b
dlebansais <dlebansais@gmail.com>
2020-09-26 19:49:38 +0200

    Removed unused PluginSettings class and interface.

--- TaskbarIconHost/Properties/AssemblyInfo.cs
+++ TaskbarIconHost/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.0.328")]
-[assembly: AssemblyFileVersion("1.0.0.265")]
+[assembly: AssemblyVersion("1.0.0.330")]
+[assembly: AssemblyFileVersion("1.0.0.266")]
 [assembly: Guid("E4801722-7212-4C7D-8949-87012B5E1B5B")]
 [assembly: NeutralResourcesLanguage("en-US")]
