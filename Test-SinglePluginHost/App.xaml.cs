48156061ac803a3fd97d580437d672710c2dcf82
dlebansais <dlebansais@gmail.com>
2020-10-11 14:40:47 +0200

    Build fix.

--- Test-SinglePluginHost/App.xaml.cs
+++ Test-SinglePluginHost/App.xaml.cs
@@ -6,6 +6,7 @@
 #pragma warning disable SA1601 // Partial elements should be documented
 #pragma warning disable CA1001 // Types that own disposable fields should be disposable
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable SA1600 // Elements should be documented
     public partial class App : Application
     {
         public App()
@@ -17,6 +18,7 @@
         private EmptyPlugin Plugin;
         private TaskbarIconHost.App PluginApp;
     }
+#pragma warning restore SA1600 // Elements should be documented
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning restore CA1001 // Types that own disposable fields should be disposable
 #pragma warning restore SA1601 // Partial elements should be documented
