d0e7ba9e4cfb44733063bca0fa3783381f31b284
dlebansais <dlebansais@gmail.com>
2020-10-12 21:46:15 +0200

    Fixed packaging.

--- SinglePluginHost/NativeMethods.cs
+++ SinglePluginHost/NativeMethods.cs
@@ -4,10 +4,10 @@
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning disable SA1600 // Elements should be documented
-    public static class NativeMethods
+    internal static class NativeMethods
     {
         [DllImport("kernel32", CharSet = CharSet.Unicode)]
-        internal static extern void OutputDebugString([In][MarshalAs(UnmanagedType.LPWStr)] string message);
+        public static extern void OutputDebugString([In][MarshalAs(UnmanagedType.LPWStr)] string message);
     }
 #pragma warning restore SA1600 // Elements should be documented
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
