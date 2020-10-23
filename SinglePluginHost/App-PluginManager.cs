a7d6a7656d27b8325d4adf6fb5060572b7d20314
dlebansais <dlebansais@gmail.com>
2020-10-23 15:04:52 +0200

    Improved code. [skip ci]

---
+++ SinglePluginHost/App-PluginManager.cs
@@ -0,0 +1,53 @@
+﻿namespace TaskbarIconHost
+{
+    using System;
+    using RegistryTools;
+
+    /// <summary>
+    /// Represents an application that can manage a plugin having an icon in the taskbar.
+    /// </summary>
+    public partial class App
+    {
+        private bool InitPlugInManager()
+        {
+            if (!PluginManager.Init(IsElevated, AssemblyName, Plugin.Guid, Owner.Dispatcher, Logger))
+                return false;
+
+            // In the case of a single plugin version, this code won't do anything.
+            // However, if several single plugin versions run concurrently, the last one to run will be the preferred one for another plugin host.
+            GlobalSettings = new RegistryTools.Settings("TaskbarIconHost", "Main Settings", Logger);
+
+            try
+            {
+                // Assign the guid with a value taken from the registry. The try/catch blocks allows us to ignore invalid ones.
+                GlobalSettings.GetString(PreferredPluginSettingName, PluginManager.GuidToString(Guid.Empty), out string PreferredPluginGuid);
+                PluginManager.PreferredPluginGuid = new Guid(PreferredPluginGuid);
+            }
+            catch
+            {
+            }
+
+            return true;
+        }
+
+        private void StopPlugInManager()
+        {
+            // Save this plugin guid so that the last saved will be the preferred one if there is another plugin host.
+            GlobalSettings?.SetString(PreferredPluginSettingName, PluginManager.GuidToString(PluginManager.PreferredPluginGuid));
+            PluginManager.Shutdown();
+
+            CleanupPlugInManager();
+        }
+
+        private void CleanupPlugInManager()
+        {
+            using (Settings? Settings = GlobalSettings)
+            {
+                GlobalSettings = null;
+            }
+        }
+
+        private const string PreferredPluginSettingName = "PreferredPlugin";
+        private Settings? GlobalSettings;
+    }
+}
---
+++ SinglePluginHost/App-Timer.cs
@@ -0,0 +1,54 @@
+﻿namespace TaskbarIconHost
+{
+    using System;
+    using System.Threading;
+    using System.Windows.Threading;
+
+    /// <summary>
+    /// Represents an application that can manage a plugin having an icon in the taskbar.
+    /// </summary>
+    public partial class App : IDisposable
+    {
+        private void InitTimer()
+        {
+            // Create a timer to display traces asynchrousnously.
+            AppTimer = new Timer(new TimerCallback(AppTimerCallback));
+            AppTimer.Change(CheckInterval, CheckInterval);
+        }
+
+        private void AppTimerCallback(object parameter)
+        {
+            // If a shutdown is started, don't show traces anymore so the shutdown can complete smoothly.
+            if (IsExiting)
+                return;
+
+            // Print traces asynchronously from the timer thread.
+            UpdateLogger();
+
+            // Also, schedule an update of the icon and tooltip if they changed, or the first time.
+            if (AppTimerOperation == null || (AppTimerOperation.Status == DispatcherOperationStatus.Completed && GetIsIconOrToolTipChanged()))
+                AppTimerOperation = Owner.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new System.Action(OnAppTimer));
+        }
+
+        private void OnAppTimer()
+        {
+            // If a shutdown is started, don't update the taskbar anymore so the shutdown can complete smoothly.
+            if (IsExiting)
+                return;
+
+            UpdateIconAndToolTip();
+        }
+
+        private void CleanupTimer()
+        {
+            using (Timer? Timer = AppTimer)
+            {
+                AppTimer = null;
+            }
+        }
+
+        private Timer? AppTimer;
+        private DispatcherOperation? AppTimerOperation;
+        private TimeSpan CheckInterval = TimeSpan.FromSeconds(0.1);
+    }
+}
