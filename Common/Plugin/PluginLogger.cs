5538de7c2ece08d5f37e2f3025fe27b669808df4
David Le Bansais <dlebansais@gmail.com>
2024-12-18 15:51:28 +0100

    Fix formatting.

--- Common/Plugin/PluginLogger.cs
+++ Common/Plugin/PluginLogger.cs
@@ -59,19 +59,19 @@ internal class PluginLogger : ILogger
     }
 
     /// <inheritdoc />
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-    {
-        Contract.RequireNotNull(formatter, out Func<TState, Exception?, string> Formatter);
-
-        Write(logLevel, Formatter(state, null));
-    }
+    public bool IsEnabled(LogLevel logLevel) => true;
 
     /// <inheritdoc />
     public IDisposable? BeginScope<TState>(TState state)
         where TState : notnull => null;
 
     /// <inheritdoc />
-    public bool IsEnabled(LogLevel logLevel) => true;
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        Contract.RequireNotNull(formatter, out Func<TState, Exception?, string> Formatter);
+
+        Write(logLevel, Formatter(state, null));
+    }
 
     /// <summary>
     /// Writes a log message.
